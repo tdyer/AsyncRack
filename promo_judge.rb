d005bf38e4aafd42104e9c719b7f7952b1d57aaf
Tom Dyer <tdyer@ourstage.com>
2012-08-22 15:06:58 -0400

    write the promo data judge clicks to DB

--- promo_judge.rb
+++ promo_judge.rb
@@ -6,6 +6,14 @@ require 'eventmachine'
 require 'em-postgres'
 require 'json'
 
+# Start server will log to log/development.log
+# thin --rackup config.ru start -p 8111
+
+# run apachebench with 1, -n 1, request and 1 user/connection, -c 1
+# send a POST request with the request params in the promo_click_data file.
+# -v 2 is verbose logging
+# ab -v 2 -n 1 -c 1 -p promo_click_data -T 'application/x-www-form-urlencoded' -H "X-Requested-With: XMLHttpRequest" http://127.0.0.1:8111/api/promo_judge/click
+
 class PromoJudge
   attr_accessor :method_to_invoke, :logger, :environment
   
@@ -64,21 +72,23 @@ class PromoJudge
       # get the DB connection
       pg = PromoJudge.conn(environment)
 
-      # query = "INSERT INTO promo_data_judge_clicks (ip, media_key, click_action, created_at, updated_at, referrer, promotion_id) VALUES (\'#{resp[:data][:ip]}\', \'#{resp[:data][:media_key]}\', \'#{resp[:data][:click_action]}\', \'#{Time.now}\', \'#{Time.now}\', \'#{@req.referrer}\', '\#{resp[:data][:promotion_id]}\');"
+      query = "INSERT INTO promo_data_judge_clicks (ip,media_key,click_action,created_at, updated_at,referrer, promotion_id) VALUES (\'#{resp[:data][:ip]}\', \'#{resp[:data][:media_key]}\',\'#{resp[:data][:click_action]}\', \'#{Time.now}\', \'#{Time.now}\', \'#{@req.referrer}\', \'#{resp[:data][:promotion_id]}\');"
+      logger.debug "PromoJudge#click: SQL = #{query.inspect}"
+      
+      # Make the non-blocking/async query, returns a EM::Deferrable
+      df = pg.execute(query)
+      logger.debug "PromoJudge#click: Executed SQL"
+      
+      # success callback
+      df.callback { |result|
+        logger.debug "PromoJudge#click: Success Callback: DB results = #{Array(result).inspect}"
+      }
 
-      # # Make the non-blocking/async query, returns a EM::Deferrable
-      # df = pg.execute(query)
-      # # success callback
-      # df.callback { |result|
-      #   logger "PromoJudge#click: DB results = #{Array(result).inspect}"
-      # }
-
-      # # error callback
-      # df.errback {|ex|
-      #   logger "PromoJudge#click: Exception = #{ex.inspect}"        
-      #   raise ex
-      # }
-
+      # error callback
+      df.errback {|ex|
+        logger.error "PromoJudge#click: Error Callback: Exception = #{ex.inspect}"        
+        raise ex
+       }
     end
     # returning this signals to the server we are sending an async
     # response
@@ -94,7 +104,6 @@ class PromoJudge
 
   def current_promotion
     # TODO: simulate ApplicationController#current_promotion in main app
-    nil
   end
 
   def promo_judge_click_url
@@ -109,7 +118,7 @@ class PromoJudge
     logger.debug "PromoJudge#action_played"
     { :data => {
         :ip=> @req.ip,
-        :promotion_id => current_promotion && current_promotion.id,
+        :promotion_id => (current_promotion && current_promotion.id) || 9999,
         :referrer => @req.referrer,
         :media_key => @req.params['media_key'],
         :click_action => 'play'
